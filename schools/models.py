5ff8f844ae35a52e37eee1cdd9d3af0b200edaf3
ludgerh <ludger@booker-hellerhoff.de>
2023-07-04 16:56:45 +0200

    0.9.15a 2023-07-04-16:56

--- schools/models.py
+++ schools/models.py
@@ -16,7 +16,7 @@ from django.db import models
 class tag(models.Model):
   name =  models.CharField(max_length=100)
   description = models.CharField(max_length=100)
-  school = models.SmallIntegerField(default=0)
+  school = models.SmallIntegerField(default=1)
   replaces = models.SmallIntegerField(default=-1)
 
   def __str__(self):
