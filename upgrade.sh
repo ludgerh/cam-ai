61d6cb60fcc8c7ec729e437054126909d7b7bb77
ludgerh <ludger@booker-hellerhoff.de>
2023-08-22 15:10:38 +0200

    1.0.1g 2023-08-22-15:10

--- upgrade.sh
+++ upgrade.sh
@@ -5,16 +5,12 @@ cp backup/camai/passwords.py cam-ai/camai/
 mv backup/env cam-ai/env
 mv backup/data cam-ai/data
 rm -rf backup
-if [ $1 = "copy" ]; then
-  cp cam-ai/upgrade.sh ./
-  echo Copy is done
-else
-  cd cam-ai
-  source env/bin/activate
-  pip install --upgrade pip
-  cp requirements.$1 requirements.txt
-  pip install -r requirements.txt
-  python manage.py migrate
-  cd ..
-  echo Upgrade is done
+cd cam-ai
+source env/bin/activate
+pip install --upgrade pip
+cp requirements.$1 requirements.txt
+pip install -r requirements.txt
+python manage.py migrate
+cd ..
+echo Upgrade is done
 fi  
