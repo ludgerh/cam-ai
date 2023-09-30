4b2214893d4d1a8fc2ba87121da284c871522daa
ludgerh <ludger@booker-hellerhoff.de>
2023-09-30 15:20:38 +0200

    1.0.3a 2023-09-30-15:

---
+++ upgrade-conda.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+validate() {
+    grep -F -q -x "$1" <<EOF
+raspi11
+raspi
+debian10
+debian12
+debian
+EOF
+}
+
+if validate $1; then
+  rm -rf backup
+  mv cam-ai backup
+  git clone https://github.com/ludgerh/cam-ai
+  cp backup/camai/passwords.py cam-ai/camai/
+  mv backup/env cam-ai/env
+  mv backup/data cam-ai/data
+  cd cam-ai
+  source ~/miniconda3/etc/profile.d/conda.sh
+  conda activate tf
+  pip install --upgrade pip
+  cp requirements.$1 requirements.txt
+  pip install -r requirements.txt
+  python manage.py migrate
+  cp upgrade.sh ..
+  chmod u+x ../upgrade.sh
+  cd ..
+  echo Upgrade is done
+else
+  echo "Usage: upgrade [OSTYPE]"
+  echo "OSTYPE in raspi11, debian10, debian12"  
+fi  
