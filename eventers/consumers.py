852276ce6405ba18a92eaade78d4a9a1c4bc5dff
derhellehof <david@hellerhoff.de>
2023-12-14 18:08:28 +0100

    D6 14.12.2023 18:08

---
+++ eventers/consumers.py
@@ -0,0 +1,20 @@
+import json
+from channels.generic.websocket import WebsocketConsumer
+from .models import alarm
+
+class alarm(WebsocketConsumer):
+
+  def connect(self):
+    if self.scope['user'].is_superuser:
+      self.accept()
+
+  def receive(self, text_data):
+    params = json.loads(text_data)['data']	
+    outlist = {'tracker' : json.loads(text_data)['tracker']}	
+
+    if params['command'] == 'write_db':
+      alarmline = alarm(name='test', mendef='text',action_type=1,action_param1='t',action_param2='t')
+      alarmline.save()
+      outlist['data'] = 'OK'
+      print('--> ' + str(outlist))
+      self.send(json.dumps(outlist))
