fd6902e284cf8117c63bef37c48d504a43d7dba6
ludgerh <ludger@booker-hellerhoff.de>
2023-12-18 11:39:14 +0100

    L31 2023-12-18-11:39

--- eventers/consumers.py
+++ eventers/consumers.py
@@ -1,6 +1,6 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
-from .models import alarm
+from .models import alarm as dbalarm
 
 class alarm(WebsocketConsumer):
 
@@ -11,9 +11,10 @@ class alarm(WebsocketConsumer):
   def receive(self, text_data):
     params = json.loads(text_data)['data']	
     outlist = {'tracker' : json.loads(text_data)['tracker']}	
+    print(text_data)
 
     if params['command'] == 'write_db':
-      alarmline = alarm(name='test', mendef='text',action_type=1,action_param1='t',action_param2='t')
+      alarmline = dbalarm(name='test', mendef='text',action_type=1,action_param1='t',action_param2='t')
       alarmline.save()
       outlist['data'] = 'OK'
       print('--> ' + str(outlist))
